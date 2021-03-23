d90d9797d0843c1d8f6461cdd170128f85d10411
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2021-03-24 01:26:53 +0300

    learn first logger, try to add log4j file

--- src/main/java/com/Epam/Alex/Entity/Entity.java
+++ src/main/java/com/Epam/Alex/Entity/Entity.java
@@ -1,3 +1,8 @@
 package com.Epam.Alex.Entity;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Entity {
+final static Logger logger= LogManager.getLogger();
 }
